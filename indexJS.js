7af967850b5d3f02a14451907d063a8f4c74de12
TheDarthFader <jhenrickson@sycamoreleaf.com>
2015-04-07 08:43:29 -0500

    JavaScript FizzBuzz example

---
+++ indexJS.js
@@ -0,0 +1,16 @@
+for(i = 1; i <= 100; i++){
+	
+	if(i % 15 == 0){
+		document.write("FIZZBUZZ! ");
+	}
+
+	if(i % 5 == 0){
+		document.write("Buzz, ");
+	}
+	
+	if(i % 3 == 0){
+		document.write("Fizz, ");
+	}else{
+		document.write(i + ', ');
+	}
+}
